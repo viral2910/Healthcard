3628027862f5b86c35a00f6e558ca00c328b5d45
vvinchhi <vvinchhi@coca-cola.com>
2022-05-19 22:51:02 +0530

    medicine changes done

---
+++ HealthCard/Medicines/PrescriptionListVC.swift
@@ -0,0 +1,29 @@
+//
+//  PrescriptionListVC.swift
+//  HealthCard
+//
+//  Created by Viral on 19/05/22.
+//
+
+import UIKit
+
+class PrescriptionListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

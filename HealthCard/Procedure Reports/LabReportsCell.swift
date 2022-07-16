006cd33505884c7744ab6b9a49f78b4c585b10f6
vvinchhi <vvinchhi@coca-cola.com>
2022-07-16 13:20:16 +0530

    cosmetic changes

---
+++ HealthCard/Procedure Reports/LabReportsCell.swift
@@ -0,0 +1,23 @@
+//
+//  LabReportsCell.swift
+//  HealthCard
+//
+//  Created by Viral on 13/07/22.
+//
+
+import UIKit
+
+class LabReportsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
